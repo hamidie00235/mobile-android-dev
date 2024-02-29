f882835f642f05a375b57725ed797a016e28decf
hamidie00235 <ahmathisseinh@gmail.com>
2024-02-29 16:14:33 +0300

    Calculator machine

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -2,30 +2,54 @@ package com.example.myfirstapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
+
+
+
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+
+import android.widget.Button;
+import android.widget.EditText;
+
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
-    private ImageView play;
+ EditText etFirstNumber,etSecondNumber;
+ Button btnAdd,btnSubtract,btnMultiply,btnDivision;
+ TextView tvResult;
 
-    @SuppressLint("MissingInflatedId")
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        etFirstNumber = findViewById(R.id.et_FirstNumber);
+        etSecondNumber = findViewById(R.id.et_SecondNumber);
+        btnAdd = findViewById(R.id.btn_Add);
+        btnSubtract = findViewById(R.id.btn_Subtract);
+        btnMultiply = findViewById(R.id.btn_Multiply);
+        btnDivision = findViewById(R.id.btn_Division);
+        tvResult = findViewById(R.id.tv_Result);
 
-        this.play=(ImageView) findViewById(R.id.play);
-
-        play.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent otherActivity = new Intent(getApplicationContext(), CookieActivity.class);
-                startActivity(otherActivity);
-                finish();
-            }
-        });
     }
-}
+    public void ButtonClick(View v) {
+        int ID = v.getId();
+        int firstNumber = Integer.parseInt(etFirstNumber.getText().toString());
+        int secondNumber = Integer.parseInt(etSecondNumber.getText().toString());
+        int result = 0;
+
+        if (ID == R.id.btn_Add) {
+            result = firstNumber + secondNumber;
+        } else if (ID == R.id.btn_Subtract) {
+            result = firstNumber - secondNumber;
+        } else if (ID == R.id.btn_Multiply) {
+            result = firstNumber * secondNumber;
+        } else if (ID == R.id.btn_Division) {
+            if (secondNumber != 0) {
+                result = firstNumber / secondNumber;
+            }
+
+         tvResult.setText("Soncuç=" +result);
+        }
+
+    }}
