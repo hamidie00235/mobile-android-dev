1c43257df78a6bfd4683b28e39c4c4db077321e7
hamidie00235 <ahmathisseinh@gmail.com>
2024-03-03 19:18:01 +0300

    Week3 Button Calculator ,age,and mass are added

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -3,14 +3,14 @@ package com.example.myfirstapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-
-
+import android.content.Intent;
 import android.os.Bundle;
 
+import android.view.View;
 import android.widget.Button;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 Button btnCalc,btnAge,btnMass;
 
 
@@ -22,4 +22,34 @@ btnCalc=findViewById(R.id.btn_Calc);
 btnAge =findViewById(R.id.btn_Age);
 btnMass=findViewById(R.id.btn_Mass);
 
-    }}
+
+btnMass.setOnClickListener(this);
+btnCalc.setOnClickListener(this);
+btnAge.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        Intent i;
+
+        if (id == R.id.btn_Calc) {
+            i = new Intent(MainActivity.this, CalculatorActivity.class);
+            startActivity(i);
+        } else if (id == R.id.btn_Mass) {
+            i = new Intent(MainActivity.this, MassActivity.class);
+            startActivity(i);
+        } else if (id == R.id.btn_Age) {
+            i = new Intent(MainActivity.this, AgeActivity.class);
+            startActivity(i);
+        }
+
+    }
+
+
+
+    }
+
+
--- app/src/main/java/com/example/myfirstapplication/MassActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MassActivity.java
@@ -2,13 +2,40 @@ package com.example.myfirstapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MassActivity extends AppCompatActivity {
+    EditText etHeight,etWeight;
+    Button btnMassCalculator;
+    TextView tvMass, tvMassInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mass);
+        etHeight=findViewById(R.id.et_Height);
+        etWeight=findViewById(R.id.et_Weight);
+        btnMassCalculator=findViewById(R.id.btn_MassCalculator);
+        tvMass=findViewById(R.id.tv_Mass);
+       tvMassInfo=findViewById(R.id.tv_MassInfo);
+       btnMassCalculator.setOnClickListener(new View.OnClickListener() {
+
+           @Override
+           public void onClick(View v) {
+               double height =Double.parseDouble(etHeight.getText().toString());
+               double weight =Double.parseDouble(etWeight.getText().toString());
+               double massIndex=weight/Math.pow(height,2);
+               tvMass.setText(  "Your mass index is: "+massIndex);
+               if(massIndex>25)
+                   tvMassInfo.setText("Obez");
+               else tvMassInfo.setText("Normal");
+           }
+       });
+
     }
 }
