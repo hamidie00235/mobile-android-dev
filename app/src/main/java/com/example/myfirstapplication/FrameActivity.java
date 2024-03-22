d9782d5a1104c289d8cd47a155f989f83e754f6b
hamidie00235 <ahmathisseinh@gmail.com>
2024-03-22 15:27:57 +0300

    week4 Frame layout is added

--- app/src/main/java/com/example/myfirstapplication/FrameActivity.java
+++ app/src/main/java/com/example/myfirstapplication/FrameActivity.java
@@ -3,12 +3,29 @@ package com.example.myfirstapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class FrameActivity extends AppCompatActivity {
+    ImageView iv1,iv2;
+    Button btnChange;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frame);
+        iv1=findViewById(R.id.iv_1);
+        iv2=findViewById(R.id.iv_2);
+        btnChange=findViewById(R.id.btn_Change);
+
+
+        btnChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                iv1.setVisibility(view.VISIBLE);
+                iv2.setVisibility(view.INVISIBLE);
+            }
+        });
     }
 }
