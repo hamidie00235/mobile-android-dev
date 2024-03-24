a7703681d5ce6b647e831e797ebc2651d2abebca
hamidie00235 <ahmathisseinh@gmail.com>
2024-03-24 03:54:39 +0300

    week5 Intend topic "share service"

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -1,20 +1,25 @@
 package com.example.myfirstapplication;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
 
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 Button btnSMSService,btnLinkService,btnMailService,btnShareServices,btnReturnActivity;
+    final static int REQUEST_CODE=1200 ;
+    TextView tvReturn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +29,7 @@ Button btnSMSService,btnLinkService,btnMailService,btnShareServices,btnReturnAct
         btnMailService=findViewById(R.id.btn_MailService);
         btnReturnActivity=findViewById(R.id.btn_ReturnActivity);
         btnShareServices=findViewById(R.id.btn_ShareService);
+        tvReturn=findViewById(R.id.tv_Return);
 
        btnSMSService.setOnClickListener(this);
         btnLinkService.setOnClickListener(this);
@@ -46,22 +52,37 @@ Button btnSMSService,btnLinkService,btnMailService,btnShareServices,btnReturnAct
         Intent i;
         if (view.getId() == R.id.btn_SMSService) {
             i = new Intent(MainActivity.this, SMSServiceActivity.class);
+            startActivity(i);
         } else if (view.getId() == R.id.btn_MailService) {
             i = new Intent(MainActivity.this, MailServiceActivity.class);
+            startActivity(i);
         } else if (view.getId() == R.id.btn_LinkService) {
             i = new Intent(MainActivity.this, LinkServiceActivity.class);
+            startActivity(i);
         } else if (view.getId() == R.id.btn_ShareService) {
             i = new Intent(MainActivity.this, ShareServicesActivity.class);
+            startActivity(i);
         } else if (view.getId() == R.id.btn_ReturnActivity) {
             i = new Intent(MainActivity.this, ReturnActivityActivity.class);
-        } else {
-            // Handle other cases or default behavior
-            return;
+            startActivityForResult(i, REQUEST_CODE);
         }
-        startActivity(i);
     }
 
-}
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==REQUEST_CODE)
+            if(resultCode== Activity.RESULT_OK) {
+                String name = data.getStringExtra("USERNAME");
+                tvReturn.setText(name);
+
+            }
+    }
+
+// Handle other cases or default behavior
+    }
+
+
 
 
 
--- app/src/main/java/com/example/myfirstapplication/ShareServicesActivity.java
+++ app/src/main/java/com/example/myfirstapplication/ShareServicesActivity.java
@@ -2,13 +2,32 @@ package com.example.myfirstapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ShareServicesActivity extends AppCompatActivity {
+    EditText etShare;
+    Button btnShare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share_services);
+        etShare=findViewById(R.id.et_Share);
+        btnShare=findViewById(R.id.btn_share);
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text=etShare.getText().toString();
+                Intent shareIntent=new Intent(Intent.ACTION_SEND) ;
+                shareIntent. setType("plain/text");
+                shareIntent.putExtra (Intent. EXTRA_TEXT, text) ;
+                startActivity(Intent.createChooser (shareIntent,"Select Platform"));
+
+            }
+        });
     }
 }
